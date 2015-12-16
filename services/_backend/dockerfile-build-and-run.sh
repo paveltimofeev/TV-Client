44d5387485513e683ae81102468487ef36d2d90e
Pavel Timofeev <pavel.timofeev@hotmail.com>
2015-12-16 22:21:05 +0400

    backend dockerized successfully

--- services/_backend/dockerfile-build-and-run.sh
+++ services/_backend/dockerfile-build-and-run.sh
@@ -1,2 +1,5 @@
+echo "ENTER PORT FOR BACKEND:"
+read port
 docker build -t backend .
-docker run -it --rm --name backend backend
+docker run -it --rm -p $port:10010/tcp -d=false --name backend backend 
+
