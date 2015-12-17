ac942a5370a2d95b99f5ea88db8137b6a6bd8077
Pavel Timofeev <pavel.timofeev@hotmail.com>
2015-12-18 00:11:57 +0400

    frontend passing basic auth parameters

--- services/_frontend/dockerfile-build-and-run.sh
+++ services/_frontend/dockerfile-build-and-run.sh
@@ -1,4 +1,9 @@
-echo "ENTER PORT FOR FRONTEND:"
+echo -n "ENTER PORT FOR FRONTEND:"
 read port
+echo -n "ENTER BASIC AUTH LOGIN:"
+read BA_LOGIN
+echo -n "ENTER BASIC AUTH PASSWORD:"
+read BA_PASSW
+
 docker build -t frontend .
-docker run -it --rm -p $port:443 -d=false --name frontend frontend 
+docker run -it --rm -p $port:443 -d=false --env BA_LOGIN="$BA_LOGIN" --env BA_PASSW="$BA_PASSW" --name frontend frontend 
--- services/_frontend/install.sh
+++ services/_frontend/install.sh
@@ -1,6 +1,8 @@
 #cp nginx.conf .nginx/conf/nginx.conf
 
 location=./.private
+usr=$1
+pas=$2
 
 mkdir $location
 
@@ -9,13 +11,6 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
    -subj "//C=UK/ST=sNYC/L=AT/O=NGINX/OU=PROXY/CN=localhost" \
    -keyout $location/ssl.key \
    -out $location/ssl.pem #-config ./openssl.cnf
-
-
-echo "SETUP BASIC AUTH"
-echo -n "SET USER NAME:"
-read usr
-echo -n "SET USER PASS:"
-read pas
 
 echo generate basic auth login/pass
 echo -n '$usr:' > $location/.htpasswd
